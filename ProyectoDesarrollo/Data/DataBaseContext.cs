5cc5fb507d6c8464c340cd002ad7619df6ca9278
PiquiJL01 <41171903+PiquiJL01@users.noreply.github.com>
2022-06-09 21:54:26 -0400

    Add project files.

---
+++ ProyectoDesarrollo/Data/DataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using ProyectoDesarrollo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace ProyectoDesarrollo.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Class> Classes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Class>().ToTable("TableName");
+        }
+    }
+}
---
+++ ProyectoDesarrollo/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using ProyectoDesarrollo.Models;
+using System;
+using System.Linq;
+
+namespace ProyectoDesarrollo.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(DataBaseContext context)
+        {
+            context.Database.EnsureCreated();
+            /*
+            // Look for any class instance in the database.
+            if (context.Classes.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var classes = new Class[]
+            {
+            new Class{Attributte1="Example1",Attributte2="Example1",DateAttribute=DateTime.Parse("2005-09-01")},
+            new Class{Attributte1="Example1",Attributte2="Example1",DateAttribute=DateTime.Parse("2005-09-01")},
+            new Class{Attributte1="Example1",Attributte2="Example1",DateAttribute=DateTime.Parse("2005-09-01")},
+            };
+            foreach (Class c in classes)
+            {
+                context.Classes.Add(c);
+            }
+            context.SaveChanges();
+            */
+        }
+    }
+}
