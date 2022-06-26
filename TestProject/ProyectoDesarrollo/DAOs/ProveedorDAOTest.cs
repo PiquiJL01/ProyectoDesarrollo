df651a5dc0847bedcc9e9cfda50a03059ae2a6e2
PiquiJL01 <41171903+PiquiJL01@users.noreply.github.com>
2022-06-25 22:15:40 -0400

    Creacion Test DAOs

---
+++ TestProject/ProyectoDesarrollo/DAOs/ProveedorDAOTest.cs
@@ -0,0 +1,66 @@
+﻿using Xunit;
+
+namespace TestProject.ProyectoDesarrollo.DAOs;
+
+public class ProveedorDAOTest
+{
+    [Fact]
+    public void Get()
+    {
+
+    }
+
+    [Fact]
+    public void GetException()
+    {
+
+    }
+
+    [Fact]
+    public void GetById()
+    {
+
+    }
+
+    [Fact]
+    public void GetByIdException()
+    {
+
+    }
+
+    [Fact]
+    public void Post()
+    {
+
+    }
+
+    [Fact]
+    public void PostException()
+    {
+
+    }
+
+    [Fact]
+    public void Put()
+    {
+
+    }
+
+    [Fact]
+    public void PutException()
+    {
+
+    }
+
+    [Fact]
+    public void Delete()
+    {
+
+    }
+
+    [Fact]
+    public void DeleteException()
+    {
+
+    }
+}
