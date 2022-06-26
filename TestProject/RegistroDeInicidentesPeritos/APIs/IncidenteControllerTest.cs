7ab8f853c36a81b99fa087ba05a8085053e1265f
PiquiJL01 <41171903+PiquiJL01@users.noreply.github.com>
2022-06-25 22:01:45 -0400

    Test APIs Registro de Incidentes

---
+++ TestProject/RegistroDeInicidentesPeritos/APIs/IncidenteControllerTest.cs
@@ -0,0 +1,66 @@
+﻿using Xunit;
+
+namespace TestProject.RegistroDeInicidentesPeritos.APIs;
+
+public class IncidenteControllerTest
+{
+    [Fact]
+    public void GetAll()
+    {
+
+    }
+
+    [Fact]
+    public void GetAllException()
+    {
+
+    }
+
+    [Fact]
+    public void GetById()
+    {
+
+    }
+
+    [Fact]
+    public void GetByIdException()
+    {
+
+    }
+
+    /*[Fact]
+    public void Post()
+    {
+
+    }
+
+    [Fact]
+    public void PostException()
+    {
+
+    }*/
+
+    [Fact]
+    public void Put()
+    {
+
+    }
+
+    [Fact]
+    public void PutException()
+    {
+
+    }
+
+    /*[Fact]
+    public void Delete()
+    {
+
+    }
+
+    [Fact]
+    public void DeleteException()
+    {
+
+    }*/
+}
