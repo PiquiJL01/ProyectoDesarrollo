60d941ef65aece74b2ba76288e9db04029039dfc
Joselyn Gonzalez <josygonzalez11@gmail.com>
2022-07-05 11:47:33 -0400

    Update IncidenteController.cs

--- Talleres/Controllers/APIs/IncidenteController.cs
+++ Talleres/Controllers/APIs/IncidenteController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using ProyectoDesarrollo.BussinesLogic.DTOs;
 using ProyectoDesarrollo.Persistence.DAO.Implementations;
-using RCVUcab;
+using ProyectoDesarrollo.Response;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
