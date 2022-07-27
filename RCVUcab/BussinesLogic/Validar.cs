c465484fc1a2b56ce8fbf5b221e3cc01c6950036
Joselyn Gonzalez <josygonzalez11@gmail.com>
2022-07-27 12:54:45 -0400

    Refactorizacion de Validar

--- RCVUcab/BussinesLogic/Validar.cs
+++ RCVUcab/BussinesLogic/Validar.cs
@@ -6,10 +6,12 @@ public static class Validar
 {
     public static bool Rol(UsuarioDTO usuarioDto)
     {
-        if ((usuarioDto.Rol == RolUsuario.Administrador) 
-            || (usuarioDto.Rol == RolUsuario.Perito)
-            || (usuarioDto.Rol == RolUsuario.Proveedor)
-            || (usuarioDto.Rol == RolUsuario.Taller))
+        bool isAdmin = (usuarioDto.Rol == RolUsuario.Administrador);
+        bool isPerito = (usuarioDto.Rol == RolUsuario.Perito);
+        bool isProveedor = (usuarioDto.Rol == RolUsuario.Proveedor);
+        bool isTaller = (usuarioDto.Rol == RolUsuario.Taller);
+
+        if (isAdmin || isPerito || isProveedor || isTaller)
         {
             return true;
         }
